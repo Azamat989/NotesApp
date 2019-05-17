4ad73b8ac12281fa4d2f5d7fe3d6a97f37bc73e0
Azamat989 <dorogov.a989@yandex.ru>
2019-05-17 22:22:12 +0300

    CreateNoteFragment and CreateTaskFragment is added

--- app/src/main/java/com/example/notes/create/CreateActivity.kt
+++ app/src/main/java/com/example/notes/create/CreateActivity.kt
@@ -1,23 +1,56 @@
 package com.example.notes.create
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.notes.R
 import com.example.notes.navigation.NavigationActivity
 import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity(), CreateNoteFragment.OnFragmentInteractionListener, CreateTaskFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        supportActionBar?.title = ""
+
         intent.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY).run {
             when {
-                this == NavigationActivity.FRAGMENT_VALUE_NOTE -> createTxtView.text = "This is a NOTE!"
-                this == NavigationActivity.FRAGMENT_VALUE_TASK -> createTxtView.text = "This is a TASK!"
-                else -> createTxtView.text = "Something goes wrong!"
+                this == NavigationActivity.FRAGMENT_VALUE_NOTE -> createFragment(CreateNoteFragment.newInstance())
+                this == NavigationActivity.FRAGMENT_VALUE_TASK -> createFragment(CreateTaskFragment.newInstance())
+                else -> "Something goes wrong!"
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+
+            R.id.saveItem -> Toast.makeText(this, "Save is clicked", Toast.LENGTH_LONG).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentHolder, fragment)
+                .commit()
+
     }
 }
